04413e7abee5c41b9ea9ea9a6438f17fb3f2e004
saande96 <49155541+saande96@users.noreply.github.com>
2020-04-30 10:29:40 -0400

    Add files via upload

---
+++ Exercises.py
@@ -0,0 +1,30 @@
+# 1. Write a Python program to count the number of even and odd numbers from a series of numbers.
+# Sample numbers : numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9)
+# Expected Output :
+# Number of even numbers : 4
+# Number of odd numbers : 5
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# 2. Write a Python program to reverse a string.
+# Sample String : "1234abcd"
+# Expected Output : "dcba4321"
